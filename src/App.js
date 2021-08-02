5842abfda49446144bb9246efceeb85543b02839
lizapincsak <liza.pincsak@gmail.com>
2021-08-01 20:30:21 -0500

    maping list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import UsersList from './components/Users/UsersList';
 import AddUser from './components/Users/AddUser';
 
 function App() {
   return (
     <div>
       <AddUser />
+      <UsersList users={[]}/>
     </div>
   );
 }
